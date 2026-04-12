3e8d0fbb2dcd3b5f1e25bdccdd34795c0814686d
Lin Bokobza <linbok123@gmail.com>
2026-04-12 14:34:17 +0300

    all

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   server: {
     port: 5173,
     strictPort: true,
+    allowedHosts: 'all',
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
